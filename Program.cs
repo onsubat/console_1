da1a452802681a2f10a86844b3db128f8fb2db5f
onsubat <m.b.altintas@hotmail.com>
2021-12-25 02:06:52 +0300

    patika deneme

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Adiniz:");
+string? isim = Console.ReadLine();
+Console.WriteLine("Soyadiniz:");
+string? soyisim = Console.ReadLine();
+Console.WriteLine("Merhaba, "+isim+" "+soyisim);
